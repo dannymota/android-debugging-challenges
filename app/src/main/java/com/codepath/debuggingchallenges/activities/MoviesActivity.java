b910db3f5826d07e21603f6d18a5bd32ad98fbcd
Danny Mota <dannymota@fb.com>
2020-06-26 12:32:01 -0400

    Fix movie adapter

--- app/src/main/java/com/codepath/debuggingchallenges/activities/MoviesActivity.java
+++ app/src/main/java/com/codepath/debuggingchallenges/activities/MoviesActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.codepath.asynchttpclient.AsyncHttpClient;
@@ -14,8 +15,10 @@ import com.codepath.debuggingchallenges.models.Movie;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import okhttp3.Headers;
 
@@ -25,7 +28,7 @@ public class MoviesActivity extends AppCompatActivity {
 
     RecyclerView rvMovies;
     MoviesAdapter adapter;
-    ArrayList<Movie> movies;
+    List<Movie> movies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,25 +36,31 @@ public class MoviesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_movies);
         rvMovies = findViewById(R.id.rvMovies);
 
+        movies = new ArrayList<>();
+
         // Create the adapter to convert the array to views
-        MoviesAdapter adapter = new MoviesAdapter(movies);
+        adapter = new MoviesAdapter(movies);
 
         // Attach the adapter to a ListView
         rvMovies.setAdapter(adapter);
+
+        rvMovies.setLayoutManager(new LinearLayoutManager(this));
 
         fetchMovies();
     }
 
 
     private void fetchMovies() {
-        String url = " https://api.themoviedb.org/3/movie/now_playing?api_key=";
+        String url = "https://api.themoviedb.org/3/movie/now_playing?api_key=" + API_KEY;
         AsyncHttpClient client = new AsyncHttpClient();
-        client.get(url, null, new JsonHttpResponseHandler() {
+        client.get(url, new JsonHttpResponseHandler() {
             @Override
-            public void onSuccess(int statusCode, Headers headers, JSON response) {
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                JSONObject jsonObject = json.jsonObject;
                 try {
-                    JSONArray moviesJson = response.jsonObject.getJSONArray("results");
-                    movies = Movie.fromJSONArray(moviesJson);
+                    JSONArray results = jsonObject.getJSONArray("results");
+                    movies.addAll(Movie.fromJSONArray(results));
+                    adapter.notifyDataSetChanged();
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
@@ -59,7 +68,7 @@ public class MoviesActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
-                Log.e(MoviesActivity.class.getSimpleName(), "Error retrieving movies: ", throwable);
+                Log.d("MoviesActivity", "onFailure" + response);
             }
         });
     }
--- app/src/main/java/com/codepath/debuggingchallenges/models/Movie.java
+++ app/src/main/java/com/codepath/debuggingchallenges/models/Movie.java
@@ -1,10 +1,13 @@
 package com.codepath.debuggingchallenges.models;
 
+import android.util.Log;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Movie {
     private String title;
@@ -13,7 +16,7 @@ public class Movie {
 
     public Movie(JSONObject jsonObject) throws JSONException {
         this.posterUrl = jsonObject.getString("poster_path");
-        this.title = jsonObject.getString("original-title");
+        this.title = jsonObject.getString("original_title");
         this.rating = jsonObject.getDouble("vote_average");
     }
 
@@ -29,15 +32,16 @@ public class Movie {
         return String.format("https://image.tmdb.org/t/p/w342/%s", posterUrl);
     }
 
-    public static ArrayList<Movie> fromJSONArray(JSONArray jsonArray) {
-        ArrayList<Movie> results = new ArrayList<>();
+    public static List<Movie> fromJSONArray(JSONArray jsonArray) throws JSONException {
+        List<Movie> results = new ArrayList<>();
+        Log.d("MovieObject", "moviesJson: " + jsonArray.length());
         for (int i = 0; i < jsonArray.length(); i++) {
-            try {
-                results.add(new Movie(jsonArray.getJSONObject(i)));
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
+            Log.d("MovieObject", jsonArray.getJSONObject(i).toString());
+            results.add(new Movie(jsonArray.getJSONObject(i)));
+            Log.d("MovieObject", "Added.");
+            Log.d("MovieObject", "Added.");
         }
+        Log.d("MovieObject", "Movies: " + results.size());
         return results;
     }
 }
